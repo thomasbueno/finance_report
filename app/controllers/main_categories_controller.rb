386b35172829a70e5fba444f1ac9c564b4e81769
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2022-11-11 20:37:17 -0300

    removing muffled data and using API requests

--- app/controllers/main_categories_controller.rb
+++ app/controllers/main_categories_controller.rb
@@ -2,6 +2,9 @@ class MainCategoriesController < ApplicationController
   EXPENSE_DATA = RestClient.get('localhost:3001/expenses', {params: {start_date: "2022-9-20", end_date: "2022-10-28"}})
   EXPENSE_DATA = JSON.parse EXPENSE_DATA
 
+  FINANCIAL_RECEIVED_DATA = RestClient.get('localhost:3002/entry/read', {params: {date_begin: "2022-09-20", date_end: "2022-10-28"} })
+  FINANCIAL_RECEIVED_DATA = JSON.parse FINANCIAL_RECEIVED_DATA
+
   def index
     if params[:type].present?
       render json: type.group_by {|item| item["category"] }
--- app/controllers/total_accounts_controller.rb
+++ app/controllers/total_accounts_controller.rb
@@ -1,54 +1,17 @@
 class TotalAccountsController < ApplicationController
 
-  EXPENSE_DATA = [
-    {
-      "id": 1,
-      "description": "Viajem de uber",
-      "category": "Transporte",
-      "value": 100,
-      "date": "2022-08-12"
-    },
-    {
-      "id": 2,
-      "description": "Viajem até o centro de São Paulo",
-      "category": "Transporte",
-      "value": 60,
-      "date": "2022-10-10"
-    },
-    {
-      "id": 3,
-      "description": "Compras do mês no mercado",
-      "category": "Alimentação",
-      "value": 600,
-      "date": "2022-11-01"
-    }
-  ]
+  EXPENSE_DATA = RestClient.get('localhost:3001/expenses', {params: {start_date: "2022-9-20", end_date: "2022-10-28"}})
+  EXPENSE_DATA = JSON.parse EXPENSE_DATA
 
-  FINANCIAL_RECEIVED_DATA = [
-    {
-      "id": 1,
-      "description": "Hora Extras",
-      "category": "Receita excepcional",
-      "value": 250,
-      "date": "2022-08-01"
-    },
-    {
-      "id": 2,
-      "description": "Salário",
-      "category": "Receita recorrente",
-      "value": 2500,
-      "date": "2022-11-01"
-    }
-  ]
-
-
+  FINANCIAL_RECEIVED_DATA = RestClient.get('localhost:3002/entry/read', {params: {date_begin: "2022-09-20", date_end: "2022-10-28"} })
+  FINANCIAL_RECEIVED_DATA = JSON.parse FINANCIAL_RECEIVED_DATA
 
   def index
-    filtered_expense = EXPENSE_DATA.filter {|expense| expense[:date] >= params[:start_date] && expense[:date] <= params[:end_date]}
-    financial_received_filtered = FINANCIAL_RECEIVED_DATA.filter {|financial_received| financial_received[:date] >= params[:start_date] && financial_received[:date] <= params[:end_date]}
+    filtered_expense = EXPENSE_DATA.filter {|expense| expense["date"] >= params["start_date"] && expense["date"] <= params["end_date"]}
+    financial_received_filtered = FINANCIAL_RECEIVED_DATA.filter {|financial_received| financial_received["date"] >= params["start_date"] && financial_received["date"] <= params["end_date"]}
 
-    total_expense = filtered_expense.map{|expense| expense[:value]}.inject(0, :+)
-    total_received = financial_received_filtered.map{|financial_received| financial_received[:value]}.inject(0, :+)
+    total_expense = filtered_expense.map{|expense| expense["value"].to_i}.inject(0, :+)
+    total_received = financial_received_filtered.map{|financial_received| financial_received["value"].to_i}.inject(0, :+)
 
     balance = total_received - total_expense
 
